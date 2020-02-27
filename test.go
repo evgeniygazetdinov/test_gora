66871c6af106017931004f87f1683760bd462c02
evgeniynokalevich <zplacesound@gmail.com>
2020-02-27 21:25:03 +0300

    !

--- test.go
+++ test.go
@@ -8,33 +8,47 @@ import (
 	"database/sql"
 	"fmt"
 )
-func main() {
-		router := mux.NewRouter().StrictSlash(true)
-		router.HandleFunc("/{id}", show_all)
-		log.Fatal(http.ListenAndServe(":8080", router))
 
+type Image struct{
+  id int
+  path string
 }
 
 
 
 
 
-func show_all(w http.ResponseWriter, r *http.Request) {
 
-		if r.Method =="GET" {
-			database, _ := sql.Open("sqllite3", "images.db")
-			rows,_ := database.Query("SELECT * images")
-			fmt.Println(rows)
+func show_all(w http.ResponseWriter, r *http.Request){
+		w.WriteHeader(http.StatusOK)
+		w.Header().Set("Content-Type", "application/json")
+		database, _ := sql.Open("sqlite3", "./images.db")
+		statement, _  := database.Prepare("CREATE TABLE IF NOT EXISTS images(id INTEGER PRIMARY KEY AUTOINCREMENT, path TEXT)")
+		statement.Exec()
+		rows, _ := database.Query("SELECT id, path from images")
+		var id int
+		var all_images []Image
+		var path string
+		for rows.Next(){
+	 	rows.Scan(&id, &path)
+		append(all_images, Image{id, path})
+		}
+			w.Write([]byte(`{"message":1)`))
+	// } else {
+	// 	w.WriteHeader(http.StatusMethodNotAllowed)
+	// 	 w.Header().Set("Content-Type", "application/json")
+	// 	 w.Write([]byte(`{"message": "method not allowed"}`))
+	// }
+}
 
 
-			w.WriteHeader(http.StatusOK)
-			w.Header().Set("Content-Type", "application/json")
-			w.Write([]byte(`{"message": 4`))
-	} else {
-		w.WriteHeader(http.StatusMethodNotAllowed)
-		 w.Header().Set("Content-Type", "application/json")
-		 w.Write([]byte(`{"message": "method not allowed"}`))
-	}
 
+
+
+
+func main() {
+		router := mux.NewRouter().StrictSlash(true)
+		router.HandleFunc("/", show_all)
+		log.Fatal(http.ListenAndServe(":8080", router))
 
 }
